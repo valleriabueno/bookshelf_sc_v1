cb65d55d64025b9c34be3ab522589cdb0b7434c6
victoricoma <victorsena3010@gmail.com>
2022-01-11 14:06:16 -0300

    Adição de serviço de genero e end point da API.

---
+++ src/app/cdd/service/generos.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenerosService {
+
+  private readonly urlAPI = '/assets/generos.json';
+
+  constructor() { }
+}
