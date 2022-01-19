910628f3c1a38c695843fa24068cb9359c21e868
victoricoma <victorsena3010@gmail.com>
2022-01-19 09:56:14 -0300

    Correção do exercício da tarefa.

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,6 +1,9 @@
+import { DashboardService } from './../servicosInterface/dashboard.service';
+import { Dashboard } from './../modelosInterface/dashboard';
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable, catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
@@ -8,26 +11,26 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
+  cards$: Observable<Dashboard[]>;
   usuario={userName: 'Victor Icoma', icone:'remember_me'};
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'O melhor livro de janeiro', img: '../../assets/imagens/1.png', cols: 1, rows: 1 },
-          { title: 'Dica dos Leitores', img: '../../assets/imagens/2.png', cols: 1, rows: 1 },
-          { title: 'O mais comentado da semana', img: '../../assets/imagens/3.png', cols: 1, rows: 1 },
-          { title: 'Indicação do time BookShelf', img: '../../assets/imagens/4.png', cols: 1, rows: 1 }
-        ];
+        return this.cards$;
       }
-
-      return [
-        { title: 'O melhor Livro de Janeiro', img: '../../assets/imagens/1.png', cols: 2, rows: 1 },
-        { title: 'Dica dos Leitores', img: '../../assets/imagens/2.png', cols: 1, rows: 1 },
-        { title: 'O mais comentado da semana', img: '../../assets/imagens/3.png', cols: 1, rows: 2 },
-        { title: 'Indicação do Time BookShelf', img: '../../assets/imagens/4.png', cols: 1, rows: 1 }
-      ];
+      return this.cards$;
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private dashboardService: DashboardService
+    ) {
+      this.cards$ = dashboardService.listagemCards()
+      .pipe(
+        catchError(error =>{
+          return of([])
+        })
+      )
+    }
 }
