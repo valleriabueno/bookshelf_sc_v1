9546ed9dfd0399a47084dbceffe11dc7e3bf7e7f
Valéria do Valle <valeriaabvalle@gmail.com>
2022-01-27 10:16:08 -0300

    Alteração firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyASIDBOJl4mWTcZ2tM_viDfM0wOTjUzd-M",
-    authDomain: "bookshelfv3-c4224.firebaseapp.com",
-    projectId: "bookshelfv3-c4224",
-    storageBucket: "bookshelfv3-c4224.appspot.com",
-    messagingSenderId: "557722969988",
-    appId: "1:557722969988:web:c303874c8c5c79ab199fd1",
-    measurementId: "G-2MR1GYR3S3"
+  apiKey: "AIzaSyDsWADOBklJobFIZmC90ArKssQq8_PyRUE",
+  authDomain: "bookshelf-51bbd.firebaseapp.com",
+  projectId: "bookshelf-51bbd",
+  storageBucket: "bookshelf-51bbd.appspot.com",
+  messagingSenderId: "27645546883",
+  appId: "1:27645546883:web:a76e0edfc48a95d1d226b6"
   },
   production: true
 };
